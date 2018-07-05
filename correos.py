a04dbddf0acef5848955f519e2b42cebdc66ddbb
stevexnx <stevexnx@gmail.com>
2018-07-05 14:57:53 +0200

    estoSepasa

--- correos.py
+++ correos.py
@@ -1,21 +1,31 @@
 import random
 lista = []
-for a in range(10):
+ciudades = []
+dire = []
+contador = 1
+contador2 = 1
+for a in range(20):
         ciudad = (random.randint(1,20))
-        direccion = (random.randint(1,100))
-        correo = [direccion,ciudad]
-  
-        for recorrido in range(1,len(correo)):
-            for posicion in range(len(correo) - recorrido):
-                if correo[posicion] > correo[posicion + 1]:
-                    temp = correo[posicion]
-                    correo[posicion] = correo[posicion + 1]
-                    correo[posicion + 1] = temp
-                    lista.append(correo)
-            for recorrido in range(1,len(correo)):        
-                for posicion in range(len(lista) - recorrido):
-                    if lista[posicion] > lista[posicion + 1]:
-                        temp = lista[posicion]
-                        lista[posicion] = lista[posicion + 1]
-                        lista[posicion + 1] = temp    
-print(lista)                                
+        direccion = (random.randint(1,1000000))
+        #correo = [direccion,ciudad]
+        ciudades.append(ciudad)
+        dire.append(direccion)
+        #dire.append(ciudades)
+        for recorrido in range(1,len(ciudades)):
+            for posicion in range(len(ciudades) - recorrido):
+                if ciudades[posicion] > ciudades[posicion + 1]:
+                    temp = ciudades[posicion]
+                    ciudades[posicion] = ciudades[posicion + 1]
+                    ciudades[posicion + 1] = temp
+                    
+            for recorrido in range(1,len(ciudades)):        
+                for posicion in range(len(dire) - recorrido):
+                    if dire[posicion] > dire[posicion + 1]:
+                        temp = dire[posicion]
+                        dire[posicion] = dire[posicion + 1]
+                        dire[posicion + 1] = temp                                             
+for i in range(19):                        
+    correo = ciudades[contador],dire[contador]   
+    print(correo)
+    contador += 1
+                       
